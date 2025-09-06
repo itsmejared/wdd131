a3960277d734dd35829456f75b8238d56baea281
Jared Huayta <jared.huayta@qualifacts.com>
2025-09-06 04:15:14 -0500

    week01 - home page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get Current year
+document.querySelector("#currentyear").innerHTML = new Date().getFullYear();
+
+// Last modified date
+document.querySelector("#lastModified").innerHTML = "Last Modified: " + document.lastModified;
